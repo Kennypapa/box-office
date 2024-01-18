8735169aed05c62b27d2665b3f0db7f1937d6219
Daramola Kehinde Joseph <81877552+Kennypapa@users.noreply.github.com>
2024-01-18 12:45:51 -0800

    updates on box-office

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,23 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import favReducer from "../features/favMovieSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";
+import { combineReducers } from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const reducer = combineReducers({
+  favorites: favReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 export const store = configureStore({
-  reducer: {
-    favorites: favReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export type AppDispatch = typeof store.dispatch;
--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,19 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import { ReactQueryProvider } from "./Provider";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
-
+let persistor = persistStore(store);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ReactQueryProvider>
-        <App />
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
       </ReactQueryProvider>
     </Provider>
   </React.StrictMode>
